33f49ed31e11ac983fae341ce3748a7a517b511f
Jose Villa <j0542420@amarillocollege.com>
2025-04-28 14:00:46 -0500

    Add project files.

---
+++ DictionaryReview/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryReview
+{
+    internal class Program
+    {
+        static Dictionary<string, int> ages = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            ages.Add("John", 25);
+            ages.Add("Jane", 30);
+            ages.Add("Bob", 35);
+
+            PrintDict();
+
+            ages = Remove("Jane");
+
+            PrintDict();
+
+            Console.ReadLine();
+        }
+
+        static void Add(string aName, int anAge)
+        {
+            ages.Add(aName, anAge);
+        }
+        static Dictionary<string, int> Remove(string aName)
+        {
+            var newDict = new Dictionary<string, int>();
+            foreach (var a in ages)
+            {
+                if (a.Key != aName) 
+                {
+                    newDict.Add(a.Key, a.Value);
+                }
+            }
+            return newDict;
+        }
+        static void PrintDict()
+        {
+            foreach (var a in ages)
+            {
+                Console.WriteLine($"{a.Key} is {a.Value} years old.");
+            }
+        }
+    }
+}
